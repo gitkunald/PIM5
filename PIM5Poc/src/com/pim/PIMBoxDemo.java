ba15bcf5851abbb1f1b77ccfda6a00b92d7e8895
gitkunald <kunal.dutta@mastechinfotrellis.com>
2023-02-20 13:05:03 +0530

    POC test

--- PIM5Poc/src/com/pim/PIMBoxDemo.java
+++ PIM5Poc/src/com/pim/PIMBoxDemo.java
@@ -21,7 +21,7 @@ public class PIMBoxDemo implements ScriptingSandboxFunction {
 		Catalog ctg = ctgMgr.getCatalog("Automation_poc_Catalog");
 		Logger logger = ctx.getLogger("ipm.log");
 		logger.logInfo("WELCOME TO AUTOMATION DEPLOYMENT ");
-		logger.logInfo("Demo Testing Ant build on 23-11-2022 : 11:38 ****** ");
+		logger.logInfo("Demo Testing Ant build on 20-02-20223 : 01:05 ****** ");
 		
 		PIMCollection<Item> allItems=ctg.getItems();
 		for(Item oitem: allItems)
